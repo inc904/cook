9cb1d1138f6cb420f3629aa39983a5cf5b549a1d
inc904 <jay1845@aliyun.com>
2022-04-12 21:57:50 +0800

    style： 类型断言

--- vue-study/v3.x/ts_v3/typescript_learn/08-type-assertion.ts
+++ vue-study/v3.x/ts_v3/typescript_learn/08-type-assertion.ts
@@ -28,12 +28,12 @@ function getName(animal: Cat | Fish) {
   return animal.name
 }
 
-function isFish(animal: Cat | Fish) {
-  if (typeof animal.swim === 'function') {
-    return true
-  }
-  return false
-}
+// function isFish(animal: Cat | Fish) {
+//   if (typeof animal.swim === 'function') {
+//     return true
+//   }
+//   return false
+// }
 function isFish2(animal: Cat | Fish) {
   if (typeof (animal as Fish).swim === 'function') {
     return true
@@ -78,3 +78,51 @@ function isApiError(error: Error) {
   }
   return false
 }
+// 上面的例子中，我们声明了函数 isApiError， 用来判断传入的参数 是不是 ApiError 类型，为了实现这样一个函数，他的参数的类型肯定是比较抽象的父类 Error，这样的话这个函数就能接受 Error 或者他的自雷作为参数了。
+
+// 利用 instanceof 完成上例
+
+function isApiError2(error: Error) {
+  if (error instanceof ApiError) {
+    return true
+  }
+  return false
+}
+// ....
+
+// ## TAG 将任何一个类型断言为 any
+
+// 访问不存在的属性或方法时，就会报错
+// const foo : number = 1
+// foo.length = 1
+// window.foo = 1  // window 上没有 foo 属性 会报错
+
+// 使用 as  any 临时将 window 断言为 any 类型
+(window as any).foo = 1
+
+// 在 any 上访问 任何属性都是被允许的。
+// 使用 any 是最后一个手段
+
+// 【它有可能掩盖了 真正的错误，所以不是非常 确定 就不要 使用 as any】
+
+// ## TAG 将 any 断言为一个类型
+
+function getCacheData(key: string): any {
+  return (window as any).cache[key]
+}
+interface Cat {
+  name: string
+  run(): void
+}
+const tom5 = getCacheData('tom') as Cat
+tom5.run()
+// 上面的例子中，我们调用完 getCacheData 之后，立即将它断言为 Cat 类型。这样的话明确了 tom 的类型，后续对 tom 的访问时就有了代码补全，提高了代码的可维护性
+
+// ##  TAG 类型短衫的限制
+
+/**
+ * - 联合类型可以被断言为其中的一个类型
+ * - 父类可以被断言成子类
+ * - 任何类型都可以被断言成 any
+ * - any 可以被断言成 任何类型
+ */
